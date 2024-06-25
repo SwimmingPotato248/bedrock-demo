8ea998d9879a0cee4b145b3ca4c3cc0c97059c48
Phong <phong.pt@cloudaz.cc>
2024-06-25 14:36:31 +0700

    Initial commit

---
+++ aws-chatbot-demo-backend/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const {
+  BedrockAgentRuntimeClient,
+  InvokeAgentCommand,
+} = require("@aws-sdk/client-bedrock-agent-runtime");
+
+const client = new BedrockAgentRuntimeClient({
+  region: "us-west-2",
+  credentials: {
+    accessKeyId: "AKIAW3MECDGYG27M674G",
+    secretAccessKey: "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",
+  },
+});
+
+const agentId = "GBANRDBZ8F";
+const agentAliasId = "IEXHF31JH9";
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: false }));
+app.use(bodyParser.json({ limit: "20mb" }));
+
+app.post("/", async (req, res) => {
+  const query = req.body.userInput;
+  const sessionId = req.body.sessionId;
+
+  const command = new InvokeAgentCommand({
+    agentId,
+    agentAliasId,
+    sessionId,
+    inputText: query,
+  });
+
+  try {
+    let completion = "";
+    let references = [];
+    const response = await client.send(command);
+
+    if (response.completion === undefined) {
+      throw new Error("Completion is undefiend");
+    }
+
+    for await (let chunkEvent of response.completion) {
+      const chunk = chunkEvent.chunk;
+      const decodedResponse = new TextDecoder("utf-8").decode(chunk.bytes);
+      completion += decodedResponse;
+      if (chunkEvent.chunk?.attribution?.citations)
+        references.push(
+          ...chunkEvent.chunk?.attribution?.citations.map(citation => {
+            return citation.retrievedReferences.map(ref => {
+              return ref.location.s3Location.uri;
+            });
+          })
+        );
+    }
+    references = Array.from(new Set(references.flat(Infinity)));
+    res.send({ completion, references });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ aws-chatbot-demo/src/App.jsx
@@ -0,0 +1,191 @@
+import { useState } from "react";
+import "./App.css";
+import Markdown from "markdown-to-jsx";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [conversation, setConversation] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentSession, setCurrentSession] = useState();
+
+  const onPrompt = async () => {
+    const sessionId = uuidv4();
+    if (!prompt) return;
+    setIsLoading(true);
+    axios({
+      url: "http://localhost:3000",
+      method: "post",
+      data: { userInput: prompt, sessionId },
+      headers: { "Referrer-Policy": "no-referrer" },
+    }).then(res => {
+      console.log(res);
+      setCurrentSession(res.data.conversation);
+      setConversation(prev => {
+        return [
+          ...prev,
+          {
+            message: "reply",
+            content: res.data.completion,
+            references: res.data.references,
+          },
+        ];
+      });
+      setIsLoading(false);
+    });
+    setConversation(prev => {
+      return [...prev, { message: "userInput", content: prompt }];
+    });
+    setPrompt("");
+    window.scrollTo(0, document.body.scrollHeight);
+  };
+  return (
+    <>
+      <div
+        style={{
+          maxWidth: "1600px",
+          width: "80%",
+          margin: "0 auto",
+          height: "80%",
+          minHeight: "800px",
+          display: "flex",
+          position: "relative",
+          flexGrow: 1,
+          flexDirection: "column",
+          border: "1px solid gray",
+        }}
+      >
+        <button onClick={() => setConversation([])}>Reset</button>
+
+        <div
+          style={{
+            width: "100%",
+            margin: "0 auto",
+            paddingBottom: "80px",
+            flexShrink: 1,
+          }}
+        >
+          {conversation.map((e, i) => {
+            if (e.message === "userInput") {
+              return (
+                <div key={i}>
+                  <p
+                    style={{
+                      backgroundColor: "#ddd",
+                      padding: "12px",
+                      fontSize: "18px",
+                    }}
+                  >
+                    {e.content}
+                  </p>
+                </div>
+              );
+            }
+            return (
+              <div key={i} style={{}}>
+                <div style={{ padding: "12px" }}>
+                  <Markdown>{e.content}</Markdown>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "30px",
+                    marginTop: "24px",
+                  }}
+                >
+                  {e.references?.map((ref, i) => {
+                    return (
+                      <div
+                        key={ref.id}
+                        style={{
+                          backgroundColor: i % 2 === 0 ? "#eee" : "#fff",
+                          padding: "12px",
+                        }}
+                      >
+                        <a
+                          href={ref}
+                          style={{
+                            textOverflow: "ellipsis",
+                            fontSize: "12px",
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                          }}
+                          target="_blank"
+                        >
+                          <span>[{i + 1}] </span>
+                          {ref}
+                        </a>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+          {isLoading && (
+            <div
+              style={{
+                padding: "12px",
+                fontSize: "18px",
+                color: "teal",
+                fontWeight: "bold",
+                height: "700px",
+              }}
+            >
+              Agent đang trả lời
+            </div>
+          )}
+        </div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            onPrompt();
+          }}
+        >
+          <div
+            style={{
+              position: "fixed",
+              bottom: "10px",
+              width: "80%",
+              maxWidth: "1600px",
+              display: "flex",
+            }}
+          >
+            <input
+              style={{
+                width: "100%",
+                borderRadius: "10px",
+                height: "3rem",
+                fontSize: "24px",
+                padding: "4px 12px",
+              }}
+              placeholder="Enter prompt here"
+              value={prompt}
+              onChange={e => setPrompt(e.target.value)}
+            />
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                style={{ height: "16px", width: "16px" }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
